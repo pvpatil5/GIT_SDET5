bbdea48043d578fe740d6bb2a2fc58dc3cac1487
pvpatil5 <pvpatil123@yahoo.com>
2022-04-14 17:14:17 +0530

    Msg

--- src/test/java/com/tyss/hyd/RMG.java
+++ src/test/java/com/tyss/hyd/RMG.java
@@ -3,7 +3,148 @@ package com.tyss.hyd;
 public class RMG {
 
 	public static void main(String[] args) {
-		System.out.println("Line added by Pavan");
+
+//		int arr[]= {4,5,7,8,2,3};
+//
+//		for (int i = 0; i < arr.length; i++) 
+//		{ 
+//			int temp=arr[arr.length-1];
+//			int temp1=arr[i];
+//			int temp3=
+//
+//			arr[arr.length-1]=arr[i];
+//
+//
+//		}
+//
+
+
+
+		//		String s1="america";
+		//		s1=s1.concat("aaa");
+		//
+		//		System.out.println(s1);
+		//
+		//		String s2 = new String("america");
+		//
+		//		String s4 = new String("america");
+		//
+		//		System.out.println(s2==s4);
+
+
+		//		
+		//		StringBuffer buffer = new StringBuffer("america");
+		//		
+		//		StringBuilder builder = new StringBuilder("america");
+		//		
+		//	
+		//		
+		//		String s3= "america";
+		//		
+		//		System.out.println(s1==s2);
+		//		
+		//		System.out.println(s1==s3);
+		//		
+
+
+		//		String s1="123@45 678";
+		//
+		//		int countdigit=0;
+		//
+		//		int countalphabet=0;
+		//
+		//		int splcharacter=0;
+		//
+		//		char[] ch = s1.toCharArray();
+		//
+		//		for (int i = 0; i < ch.length; i++) 
+		//		{
+		//			if(!Character.isDigit(ch[i]) && !Character.isAlphabetic(ch[i]) && !Character.isSpaceChar(ch[i])) 
+		//			{
+		//				System.out.println(ch[i]);
+		//				splcharacter++;
+		//			}
+		//			else if (Character.isAlphabetic(ch[i]))
+		//			{
+		//				countalphabet++;
+		//			}
+		//			else {
+		//				System.out.println();
+		//			}
+		//
+		//		}
+		//		if(countdigit>=1 && countalphabet>=1)
+		//		{
+		//			System.out.println("Word if alpha numeric");	
+		//		}
+		//
+		//		else if(countdigit>=1 && countalphabet>=0) {
+		//			System.out.println("Alphabet not present");
+		//		}
+		//		else if(countdigit>=0 && countalphabet>=1) {
+		//			System.out.println("Number not present");
+		//		}
+		//		else if (splcharacter>=1){
+		//			System.out.println("spl character present");
+		//		}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+		//		String s1= "hi hello how are you";
+		//		String s2 = s1.replace(" ", "");
+		//		char ch[]=	s2.toCharArray();
+		//
+		//		int ch[]= {1,2,4,7,2,3,5,6,4,2,1};
+		//		int	countpairs=0;
+		//		for (int i = 0; i < ch.length; i++) 
+		//		{
+		//			for (int j = i+1; j < ch.length; j++) 
+		//			{
+		//				if(ch[i]+ch[j]==8) 
+		//				{
+		//					countpairs++;
+		//					System.out.println(ch[i]+"  "+ch[j]);
+		//				}
+		//			}
+		//		}
+		//		System.out.println(countpairs);
+
+
+
+
+
+
+
+		//		for (int i = 0; i < ch.length; i++) 
+		//		{
+		//			int count=1;
+		//			for (int j = i+1; j < ch.length; j++) 
+		//			{
+		//				if(ch[i]==ch[j]) 
+		//				{
+		//					ch[j]='0'; 
+		//					count++;
+		//				}
+		//
+		//			}	
+		//			if(ch[i]!='0' && count>1) {
+		//				System.out.println(ch[i]+"="+ count);
+		//			}
+		//		}
 
 	}
 
